38f4359f310653b9cad05c422c47f495d61fd8ff
Youhao Gong 宫酉昊 <youhao.gong.o@nio.com>
2018-04-18 13:08:02 +0800

    0.0.11

--- Pay.podspec
+++ Pay.podspec
@@ -16,7 +16,7 @@ Pod::Spec.new do |s|
   #
 
   s.name         = "Pay"
-  s.version      = "0.0.10"
+  s.version      = "0.0.11"
   s.summary      = "蔚来汽车"
 
   # This description is used to generate tags and improve search results.
@@ -106,7 +106,8 @@ Pod::Spec.new do |s|
   #
 
   # s.resource  = "icon.png"
-  s.resources = "Pay/*.bundle"
+
+  s.resources = "Pay/Sources/*.xib"
 
   # s.preserve_paths = "FilesToSave", "MoreFilesToSave"
 
--- Pay/Sources/APaymentController.swift
+++ Pay/Sources/APaymentController.swift
@@ -8,22 +8,22 @@
 
 import UIKit
 
-public class APaymentController: UIViewController {
+class APaymentController: UIViewController {
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
     }
     
-    required public init?(coder aDecoder: NSCoder) {
+    required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    override public func viewDidLoad() {
+    override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
-    override public func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
