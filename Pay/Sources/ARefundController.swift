554b33cac3a0210615c780914dab37dfc63933a8
Youhao Gong 宫酉昊 <youhao.gong.o@nio.com>
2018-04-18 13:41:54 +0800

    0.0.12

--- Pay/Sources/ARefundController.swift
+++ Pay/Sources/ARefundController.swift
@@ -1,14 +1,14 @@
 //
-//  APaymentController.swift
+//  ARefundController.swift
 //  CentralManager
 //
-//  Created by Youhao Gong 宫酉昊 on 2018/4/17.
+//  Created by Youhao Gong 宫酉昊 on 2018/3/22.
 //  Copyright © 2018年 Youhao Gong 宫酉昊. All rights reserved.
 //
 
 import UIKit
 
-public class APaymentController: UIViewController {
+public class ARefundController: UIViewController {
     override public init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
     }
