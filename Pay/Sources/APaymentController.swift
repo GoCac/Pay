ec95edad3d035fff1e56646b6f9ad42851772b4f
Youhao Gong 宫酉昊 <youhao.gong.o@nio.com>
2018-04-18 11:44:40 +0800

    0.0.9

--- Pay/Sources/APaymentController.swift
+++ Pay/Sources/APaymentController.swift
@@ -8,22 +8,22 @@
 
 import UIKit
 
-class APaymentController: UIViewController {
+public class APaymentController: UIViewController {
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
     }
     
-    required init?(coder aDecoder: NSCoder) {
+    required public init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    override func viewDidLoad() {
+    override public func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
+    override public func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
--- Pay/Sources/APaymentControllerRefundController.swift
+++ Pay/Sources/APaymentControllerRefundController.swift
@@ -8,21 +8,21 @@
 
 import UIKit
 
-class ARefundController: UIViewController {
+public class ARefundController: UIViewController {
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
     }
     
-    required init?(coder aDecoder: NSCoder) {
+    required public init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    override func viewDidLoad() {
+    override public func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
+    override public func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
